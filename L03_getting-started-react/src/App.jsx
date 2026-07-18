d194176c23c31e0035292593af0b83a266108c15
Brenan Lester Espeleta <brenanlesterespeleta05@gmail.com>
2026-07-18 15:00:48 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Brenan Lester P. Espeleta</h1>
+      <h2>BSIS - 3</h2>
+      <ol>
+        <li>My laziness cannot be fixed — just like a bug. It will reappear no matter what. 🐛</li>
+      </ol>
+    <hr />
+    <p>What i learned so far on this course, is the basic and complex Git Commands that a developer necessarily uses. 
+      I also learned about how you start a project on React and also why React can be used both on web and app development. 
+      I really found it interesting, how a complex language like React works. I hope to learn more about React.
+      React is hard to comprehend at first.</p>
+    </>
+  )
+}
+
+export default App
